eb610e9adb30024f92194212e681efd7a54628e9
Akanteruk <kanterukt@gmail.com>
2017-12-19 10:13:51 +0200

    add function createCompany() to UsersDAOSpec

--- test/com/dataengi/crm/identities/daos/UsersDAOSpec.scala
+++ test/com/dataengi/crm/identities/daos/UsersDAOSpec.scala
@@ -1,15 +1,21 @@
 package com.dataengi.crm.identities.daos
 
+
 import com.mohiva.play.silhouette.api.LoginInfo
 import com.dataengi.crm.common.context.types._
 import com.dataengi.crm.common.extensions.awaits._
 import com.dataengi.crm.common.extensions.logging._
 import com.dataengi.crm.configurations.RolesConfiguration
-import com.dataengi.crm.identities.context.{AuthenticationContext, CompaniesServiceContext, RolesServiceContext}
-import com.dataengi.crm.identities.models.{User, UserStates}
+import com.dataengi.crm.identities.context.{
+  AuthenticationContext,
+  CompaniesServiceContext,
+  RolesServiceContext
+}
+import com.dataengi.crm.identities.models.{User, UserStates, Company}
 import org.scalacheck.Gen
 import org.specs2.runner.SpecificationsFinder
 import play.api.test.PlaySpecification
+
 
 class UsersDAOSpec
     extends PlaySpecification
@@ -24,51 +30,60 @@ class UsersDAOSpec
     )
   }
 
-  val TestLoginInfo: LoginInfo  = LoginInfo("provider", "dasd@asd.com")
-  val TestLoginInfo2: LoginInfo = LoginInfo("provider2", "dasd2@asd.com")
   lazy val usersDAO             = application.injector.instanceOf[UsersDAO]
+  def createCompany(): Company = {
+    val NewCompanyName: String          = "NEW_COMPANY" + Gen.alphaStr.sample.get
+    val newCompanyResult: XorType[Long] = companiesService.create(NewCompanyName).await()
+    val company: Company                = companiesService.get(newCompanyResult.value).await().value
+
+    company.id must not be None
+
+    company
+  }
 
   "UsersDAOSpecification" should {
 
     "add user" in {
-      val NewCompanyName: String          = "NEW_COMPANY" + Gen.alphaStr.sample.get
-      val newCompanyResult: XorType[Long] = companiesService.create(NewCompanyName).await()
-      val newCompany                      = companiesService.get(newCompanyResult.value).await().value
-      val role                            = rolesService.find(RolesConfiguration.CompanyManager.name).await().value
 
-      val user = User(
+//      val user: User = createUserWithRole("user1","user1@test.com").await().right.get
+
+      val role    = rolesService.find(RolesConfiguration.CompanyManager.name).await().value
+      val company = createCompany()
+      val TestLoginInfo: LoginInfo  = LoginInfo("provider", "dasd@asd.com")
+      val TestLoginInfo2: LoginInfo = LoginInfo("provider2", "dasd2@asd.com")
+      val user: User = User(
         loginInfo = TestLoginInfo,
-        company = newCompany,
+        company = company,
         role = role
       )
 
       val getUserResult = usersDAO.add(user).await()
       println(s"[user-dao][get] ${getUserResult.logResult}")
       getUserResult.isRight === true
+
     }
 
     "add userList" in {
-      val NewCompanyName: String          = "NEW_COMPANY" + Gen.alphaStr.sample.get
-      val newCompanyResult: XorType[Long] = companiesService.create(NewCompanyName).await()
-      val newCompany                      = companiesService.get(newCompanyResult.value).await().value
+      val company = createCompany()
       val role                            = rolesService.find(RolesConfiguration.CompanyManager.name).await().value
-
+      val TestLoginInfo: LoginInfo  = LoginInfo("provider", "dasd@asd.com")
+      val TestLoginInfo2: LoginInfo = LoginInfo("provider2", "dasd2@asd.com")
       val user = User(
         loginInfo = TestLoginInfo,
-        company = newCompany,
+        company = company,
         role = role
       )
       val user2 = User(
         loginInfo = TestLoginInfo2,
-        company = newCompany,
+        company = company,
         role = role
       )
 
       val userList = List(user, user2)
 
-      newCompany.id must not be None
+      company.id must not be None
 
-      val companyId = newCompany.id.get
+      val companyId = company.id.get
 
       val getUserResult = usersDAO.add(userList).await()
       println(s"[user-dao][get] ${getUserResult.logResult}")
@@ -84,6 +99,9 @@ class UsersDAOSpec
     }
 
     "find user by login info" in {
+
+      val TestLoginInfo: LoginInfo  = LoginInfo("provider", "dasd@asd.com")
+
       val findUserResult = usersDAO.find(TestLoginInfo).await()
       println(s"[user-dao][find] ${findUserResult.logResult}")
       findUserResult.isRight === true
@@ -92,14 +110,12 @@ class UsersDAOSpec
     }
 
     "find user by companyId negative" in {
-      val NewCompanyName: String          = "NEW_COMPANY" + Gen.alphaStr.sample.get
-      val newCompanyResult: XorType[Long] = companiesService.create(NewCompanyName).await()
-      val newCompany                      = companiesService.get(newCompanyResult.value).await().value
+      val company = createCompany()
       val role                            = rolesService.find(RolesConfiguration.CompanyManager.name).await().value
 
-      newCompany.id must not be None
+      company.id must not be None
 
-      val companyId      = newCompany.id.get
+      val companyId      = company.id.get
       val findUserResult = usersDAO.findByCompany(companyId).await()
       println(s"[user-dao][find] ${findUserResult.logResult}")
       findUserResult.isRight === true
@@ -117,9 +133,7 @@ class UsersDAOSpec
     }
 
     "update user" in {
-      val NewCompanyName: String          = "NEW_COMPANY" + Gen.alphaStr.sample.get
-      val newCompanyResult: XorType[Long] = companiesService.create(NewCompanyName).await()
-      val newCompany                      = companiesService.get(newCompanyResult.value).await().value
+      val company = createCompany()
       val role                            = rolesService.find(RolesConfiguration.CompanyManager.name).await().value
 
       val TestLoginInfo3: LoginInfo = LoginInfo("provider3", "dasd3@asd.com")
@@ -127,7 +141,7 @@ class UsersDAOSpec
 
       val user3 = User(
         loginInfo = TestLoginInfo3,
-        company = newCompany,
+        company = company,
         role = role
       )
 
@@ -137,7 +151,7 @@ class UsersDAOSpec
 
       val key = getUserResult.right.get
 
-      val updateUser       = User(loginInfo = TestLoginInfo4, company = newCompany, role = role).copy(id = Some(key))
+      val updateUser       = User(loginInfo = TestLoginInfo4, company = company, role = role).copy(id = Some(key))
       val updateUserResult = usersDAO.update(updateUser).await()
       updateUserResult.isRight === true
 
@@ -148,28 +162,25 @@ class UsersDAOSpec
     }
 
     "update user negative" in {
-      val NewCompanyName: String          = "NEW_COMPANY" + Gen.alphaStr.sample.get
-      val newCompanyResult: XorType[Long] = companiesService.create(NewCompanyName).await()
-      val newCompany                      = companiesService.get(newCompanyResult.value).await().value
+      val company = createCompany()
       val role                            = rolesService.find(RolesConfiguration.CompanyManager.name).await().value
 
       val TestLoginInfo5: LoginInfo = LoginInfo("provider4", "dasd4@asd.com")
 
-      val updateUser       = User(loginInfo = TestLoginInfo5, company = newCompany, role = role).copy(id = Some(56667))
+      val updateUser       = User(loginInfo = TestLoginInfo5, company = company, role = role,id = Some(56667))
       val updateUserResult = usersDAO.update(updateUser).await()
       updateUserResult.isLeft === true
 
     }
 
     "getOption user" in {
-      val NewCompanyName: String          = "NEW_COMPANY" + Gen.alphaStr.sample.get
-      val newCompanyResult: XorType[Long] = companiesService.create(NewCompanyName).await()
-      val newCompany                      = companiesService.get(newCompanyResult.value).await().value
+      val company = createCompany()
       val role                            = rolesService.find(RolesConfiguration.CompanyManager.name).await().value
 
+      val TestLoginInfo: LoginInfo  = LoginInfo("provider", "dasd@asd.com")
       val user = User(
         loginInfo = TestLoginInfo,
-        company = newCompany,
+        company = company,
         role = role
       )
 
@@ -186,16 +197,14 @@ class UsersDAOSpec
     }
 
     "update state user" in {
-      val NewCompanyName: String          = "NEW_COMPANY" + Gen.alphaStr.sample.get
-      val newCompanyResult: XorType[Long] = companiesService.create(NewCompanyName).await()
-      val newCompany                      = companiesService.get(newCompanyResult.value).await().value
+      val company = createCompany()
       val role                            = rolesService.find(RolesConfiguration.CompanyManager.name).await().value
 
       val TestLoginInfo: LoginInfo = LoginInfo("provider", "dasd@asd.com")
 
       val user = User(
         loginInfo = TestLoginInfo,
-        company = newCompany,
+        company = company,
         role = role
       )
 
